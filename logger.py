9103e3602c40c2f468597de7e64324a20ad21c47
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2025-06-25 03:17:33 +0000

    added filelogger

--- logger.py
+++ logger.py
@@ -3,8 +3,28 @@ from rich.console import Console
 class consoleLog:
     ...
 class fileLog:
-    ...
+    def log(self):
+        time = datetime.now().strftime("%H:%M:%S")
+        date = datetime.now().strftime("%d-%m-%Y")
+        with open(f'{date} {time}.log', 'a') as file:
+            file.write(f"[INFO {time}] {self}")
+    def warn(self):
+        time = datetime.now().strftime("%H:%M:%S")
+        date = datetime.now().strftime("%d-%m-%Y")
+        with open(f'{date} {time}.log', 'a') as file:
+            file.write(f"[WARN {time}] {self}")
+    def error(self):
+        time = datetime.now().strftime("%H:%M:%S")
+        date = datetime.now().strftime("%d-%m-%Y")
+        with open(f'{date} {time}.log', 'a') as file:
+            file.write(f"[ERROR {time}] {self}")
+    def fatal(self):
+        time = datetime.now().strftime("%H:%M:%S")
+        date = datetime.now().strftime("%d-%m-%Y")
+        with open(f'{date} {time}.log', 'a') as file:
+            file.write(f"[!!!FATAL ERROR!!! {time}] {self}")
 
 """
 TODO: add file operations inside the log foler, maybe initialize the directory?
+TODO: Auth users with bcrypt (let's only use the "root" user for now, for simplicity's sake)
 """
