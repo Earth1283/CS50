3a815ccd2e60b1bfcd2c4a36e88edc38e2d6bb97
Joseph Cui <joseph_cio@hotmail.com>
2025-06-28 16:03:58 +0800

    fixed log file bug

--- logger.py
+++ logger.py
@@ -1,25 +1,26 @@
 from datetime import datetime
-from rich.console import Console
+from typing import Optional
+def initializeFileName():
+    time = datetime.now().strftime("%H:%M:%S")
+    date = datetime.now().strftime("%Y-%m-%d")
+    global filename
+    filename = f"{time} {date}.log"
 class consoleLog:
     ...
 class fileLog:
-    def log(self):
+    def log(self: str):
         time = datetime.now().strftime("%H:%M:%S")
-        date = datetime.now().strftime("%d-%m-%Y")
-        with open(f'logs/{date} {time}.log', 'a') as file:
+        with open(f'{filename}.log', 'a') as file:
             file.write(f"[INFO {time}] {self}\n")
-    def warn(self):
+    def warn(self: str):
         time = datetime.now().strftime("%H:%M:%S")
-        date = datetime.now().strftime("%d-%m-%Y")
-        with open(f'logs/{date} {time}.log', 'a') as file:
+        with open(f'{filename}.log', 'a') as file:
             file.write(f"[WARN {time}] {self}\n")
-    def error(self):
+    def error(self: str):
         time = datetime.now().strftime("%H:%M:%S")
-        date = datetime.now().strftime("%d-%m-%Y")
-        with open(f'logs/{date} {time}.log', 'a') as file:
+        with open(f'{filename}.log', 'a') as file:
             file.write(f"[ERROR {time}] {self}\n")
-    def fatal(self):
+    def fatal(self: str):
         time = datetime.now().strftime("%H:%M:%S")
-        date = datetime.now().strftime("%d-%m-%Y")
-        with open(f'logs/{date} {time}.log', 'a') as file:
+        with open(f'{filename}.log', 'a') as file:
             file.write(f"[!!!FATAL ERROR!!! {time}] {self}\n")
