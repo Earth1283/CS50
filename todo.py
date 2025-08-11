955e0e0c518d437a36ddb4c016c77b6608cacff1
Joseph Cui <joseph_cio@hotmail.com>
2025-08-11 17:50:24 +0800

    update todo

--- todo.py
+++ todo.py
@@ -33,8 +33,32 @@ class toDo():
         console.print(f"{option1}\n{option2}\n{option3}\n{option4}\n\n{quit}")
         desiredFunction = int(0)
         while not validated(desiredFunction):
-            desiredFunction = input("Please enter your desired function\n▶ ").strip()
+            try:
+                desiredFunction = input("Please enter your desired function\n▶ ").strip()
+                validated(desiredFunction)
+            except ValueError:
+                console.print("[bold]Please enter a valid option![/bold]", style="#FF0000")
+                continue
             continue # stupid but works ig
+        match desiredFunction:
+            case 1:
+                console.print("You may enter your new task name below:", style="#00AAFF")
+                placeholderAnswer = "looooooooooooooooooooooooooooooooooooooooooooooooooooooongAnswer"
+                userAnswer = placeholderAnswer
+                tempArray = [] # store the function for now (use csv writer later)
+                while len(userAnswer)>40:
+                    userAnswer = input("▶ ").strip()
+                    console.print("Please refrain from using excessively long titles", style="#FF0000")
+                    continue
+                tempArray = tempArray.append(userAnswer)
+                # that's it for the first column
+                userAnswer = placeholderAnswer
+                console.print("Now you may enter your task details below:")
+                while len(userAnswer)>100:
+                    userAnswer = input("▶ ").strip()
+                    console.print(f"Please refrain cerating task descriptions above 100 characters")
+                    console.print(f"Your input was {len(userAnswer)} characters long")
+                    continue
 
 def initialize():
     toDo.main()
