b8d14d29488dedd65572d6dd9d59d668501aa275
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2025-10-13 02:30:01 +0000

    style choices

--- sys_info.py
+++ sys_info.py
@@ -1,11 +1,14 @@
 from logger import file_log as fl, LogLevel
 import platform
+
 def getOS():
     fl.logger(LogLevel.INFO, "Getting OS info from API call")
     return platform.system()
+
 def getPythonVersion():
     fl.logger(LogLevel.INFO, "Getting Python Version info from API call")
     return platform.python_version()
+
 def getMachine():
     fl.logger(LogLevel.INFO, "Getting machine info from API call")
     return platform.machine()
